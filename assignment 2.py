f658b9c8a29866716382f87364261c482fb30824
madhavahuja <madhavahuja2@gmail.com>
2018-06-14 07:55:18 +0530

    Assignment 2 completed

---
+++ assignment 2.py
@@ -0,0 +1,27 @@
+#q 1
+print("Hello")
+
+#q 2
+print("acad" + "view")
+
+#q 3
+
+x=10
+y=20
+z=30
+print(x,y,z)
+
+#q 4
+
+print('Let\'s get started')
+
+#q 5
+s="Acadview"
+course="Python"
+fees=5000
+print("welcolme to %s your course is %s and fees is %d"%(s,course,fees))
+
+#q 6
+name="Tony Stark"
+salary=1000000
+print("%s %d"%(name,salary))
